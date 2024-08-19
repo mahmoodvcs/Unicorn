eba94727fbbc88469b932a8f32baab11381750a3
mahmoodvcs <mahmood.dehghan@gmail.com>
2024-08-19 21:18:42 +1000

    Upgrade project files to SDK style

--- Unicorn.Web/Properties/AssemblyInfo.cs
+++ Unicorn.Web/Properties/AssemblyInfo.cs
@@ -3,18 +3,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Web.UI;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Unicorn.Web")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unicorn.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -22,18 +10,6 @@ using System.Web.UI;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("80a64eea-4ca8-4ee9-86be-11ecdeaa85b8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: WebResource("Unicorn.Web.ClientControl1.js", "text/javascript")]
 [assembly: ScriptResource("Unicorn.Web.ClientControl1.js",
    "Unicorn.Web.ClientControl1", "Unicorn.Web.Resource")]
